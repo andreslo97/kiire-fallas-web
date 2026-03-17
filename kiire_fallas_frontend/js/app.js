f43b2046682cd2a55e62043db60a880e8b10dcfc
Juan Andrés Loaiza <juanandres785@gmail.com>
2026-03-17 18:19:25 -0500

    Connect frontend to production API

--- kiire_fallas_frontend/js/app.js
+++ kiire_fallas_frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://kiire-fallas-api.onrender.com";
 
 const ticketForm = document.getElementById("ticketForm");
 const mensaje = document.getElementById("mensaje");
